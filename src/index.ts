d67744290cd9356ee131c02908055c481e1f191c
sk <sk@sks-MacBook-Pro.local>
2025-12-28 16:22:21 +0530

    integration test using jest and supertest

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import express, {
 import dotenv from 'dotenv';
 import connectDB from './startup/db';
 import routes from './startup/routes';
-dotenv.config();
+import './config/env';
 const app: Express = express();
 routes(app);
 connectDB();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
    console.log(`Server is running on port ${port}`);
 });
+export default server;
---
+++ src/tests/integration/genres.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest';
+import { Genre } from '../../models/genre';
+import { User } from '../../models/user';
+import server from '../../index';
+import mongoose from 'mongoose';
+
+describe('/api/genres', () => {
+  afterAll(async () => {
+    await server.close();
+    await mongoose.disconnect();
+  });
+
+  afterEach(async () => {
+    await Genre.deleteMany({});
+  });
+
+  describe('GET /', () => {
+
+    console.log(process.env.NODE_ENV)
+    it('should return all genres', async () => {
+      await Genre.collection.insertMany([
+        { name: 'genre1' },
+        { name: 'genre2' },
+      ]);
+
+      const token = new User({ isAdmin: false, name: 'Test User', email: 'test@test.com' }).generateAuthToken();
+
+      const res = await request(server)
+        .get('/api/genres')
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g: any) => g.name === 'genre1')).toBeTruthy();
+      expect(res.body.some((g: any) => g.name === 'genre2')).toBeTruthy();
+    });
+  });
+});
